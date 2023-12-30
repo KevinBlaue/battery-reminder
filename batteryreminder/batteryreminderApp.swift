07a3a527e018246741c73b75db85dadeb863215a
Kevin Blaue <kevin@blaue-online.com>
2023-12-30 18:22:09 +0100

    Initial Commit

---
+++ batteryreminder/batteryreminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  batteryreminderApp.swift
+//  batteryreminder
+//
+//  Created by Kevin Blaue on 30.12.23.
+//
+
+import SwiftUI
+
+@main
+struct batteryreminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
